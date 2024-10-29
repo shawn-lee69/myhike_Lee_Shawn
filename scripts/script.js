8c4c96cfb1c3144b1408907e14f0bb919742c17d
Shawn Lee <slee1109@my.bcit.ca>
2024-10-29 14:15:18 -0700

    added codes for demo09

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,14 @@
 //---------------------------------
 // Your own functions here
 //---------------------------------
-
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+      }).catch((error) => {
+        // An error happened.
+      });
+}
 
 function sayHello() {
     //do something
